db61a962e7c7d5d96e0f84a9a22afa5cc0392100
Mia Sundah <miasundah23@gmail.com>
2023-02-16 00:51:06 +0800

    modify line import.js

--- group-ex2/import.js
+++ group-ex2/import.js
@@ -21,8 +21,8 @@ Alasan menggunakan Module
 2.Penggunaan nama variable
 3.Reusable Code
 
-Module ini tidak bisa dijalankan jika akses file .html menggunakan url direktori local, tapi kita harus
-menggunakan Live Server
+Module ini tidak bisa dijalankan apabila akses file .html menggunakan url direktori local, tapi kita harus
+menggunakan Live Server.
 */
 
 /*
@@ -32,17 +32,17 @@ menggunakan Live Server
     'import' juga merupakan bagian dari module pada javaScript ES6, dianggap sebagai pasangan ekspor, karena fungsinya untuk menggunakan data yang di ekspor dari module lainnya. Sama halnya dengan ekspor, import juga bisa dilakukan terpisah atau digabung.
 */
 
-//Import anggota
+//Import Anggota
 import { anggota } from "./export.js";
 
 //Output dari anggota
 console.log("Nama-nama anggota Tokyo: ",...anggota); // Output: Nama-nama anggota Tokyo:  nat mia hen enji cher sher
 
-//import default
+//Import Default
 import aku from "./export.js";//menampilkan hasil export.js
 aku("Nataliaaaaa");
 
-// import variabel
+// Import Variabel
 import { angka, sentence, Book, FuncBook } from "./export.js";
 
 
@@ -59,14 +59,14 @@ FuncBook(Book.judul, Book.penulis, Book.penerbit, Book.tahun_terbit, Book.jumlah
 
 
 /*
-    import as
+    Import As
     
     Sama seperti export as, kita bisa melakukan import dengan tujuan mengganti nama suatu variabel, objek, fungsi dengan menggunakan import as.
 */
 
 // Kita ambil contoh untuk ubah import dari ekspor variabel diatas dengan menggunakan import as.
 
-// import { angka, sentence, Book, FuncBook } from "./export.js";
+// Import { angka, sentence, Book, FuncBook } from "./export.js";
 // ini tanpa menggunakan import as. Kita modify code diatas menggunakan import as.
 
 import { angka as number, sentence as kalimat, Book as objek } from "./export.js";
@@ -78,7 +78,7 @@ console.log(kalimat);
 console.log(objek);
 console.log("-------------------------------USE IMPORT AS----");
 
-// import variabel yang di modify dengan 'as'
+// Import variabel yang di modify dengan 'as'
 
 import {num1, num2} from "./export.js";
 
